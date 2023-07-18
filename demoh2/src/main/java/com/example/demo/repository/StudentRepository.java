bdb5388dd3883a71f097f1ae7737c41eaf44628d
manjula-bhinoy <139793864+manjula-bhinoy@users.noreply.github.com>
2023-07-18 15:05:32 +0300

    Delete demoh2 directory

--- demoh2/src/main/java/com/example/demo/repository/StudentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.repository.CrudRepository;  
-import com.example.demo.entity.Student;  
-public interface StudentRepository extends CrudRepository<Student, Long>  
-{  
-}
